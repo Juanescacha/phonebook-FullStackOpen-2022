4585f01294aec6aed1fcb79452862b22c87df2e7
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2022-07-27 16:02:41 -0500

    New Build for Frontend + Backend Logic for MongoDB

--- index.js
+++ index.js
@@ -18,55 +18,83 @@ morgan.token("post", function (req) {
     return " "
   }
 })
+
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :post")
 )
 
+// GET info
+
 app.get("/info", (request, response) => {
-  response.send(
+  /* response.send(
     `phonebook has info for ${persons.length} people <br> ${new Date()}`
-  )
+  ) */
+
+  Person.find({})
+    .then(persons => {
+      response.send(
+        `phonebook has info for ${persons.length} people <br> ${new Date()}`
+      )
+    })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+// GET persons
+
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+// GET person
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.deleteOne({ id: request.params.id }).then(res => {
-    response.status(204).end()
-  })
+// DELETE person
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+// POST person
+
+app.post("/api/persons", (request, response, next) => {
   const body = request.body
 
-  if (body.name === undefined) {
+  // Revisa que el objeto no tenga informacion vacia
+
+  if (body.name === undefined || body.name === "") {
     return response.status(400).json({
       error: "name missing",
     })
   }
 
-  if (body.number === undefined) {
+  if (body.number === undefined || body.number === "") {
     return response.status(400).json({
       error: "number missing",
     })
   }
 
-  /* Not yet implemented making sure name dont already exits
-  if (persons.filter(person => person.name === body.name).length > 0) {
-    return response.status(400).json({
-      error: "name must be unique",
-    })
-  } */
+  /* if (persons.filter(person => person.name === body.name).length > 0) {return response.status(400).json({      error: "name must be unique",})} */
+
+  // New Object to Db
 
   const person = new Person({
     id: Math.floor(Math.random() * 1000),
@@ -74,10 +102,46 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person
+    .save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
+
+// PUT person
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+// Error Handling Middleware
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+// App listening at port for HTTP requests
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
